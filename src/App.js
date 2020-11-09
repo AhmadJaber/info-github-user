655ffdaf67449d9a1850225859198b8f75921fa7
AhmadJaber <jaber131706@gmail.com>
2020-11-09 19:41:26 +0600

    :sparkles: routing added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import { Dashboard, Login, Error } from "./pages";
+
 function App() {
-  return <div className="App">hello!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="*" exact>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
