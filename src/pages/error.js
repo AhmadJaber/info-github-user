5ec3164c086d551e7ebd136c53e63261032ebb66
AhmadJaber <jaber131706@gmail.com>
2020-12-28 09:57:59 +0600

    :hammer: error page edited

--- src/pages/error.js
+++ src/pages/error.js
@@ -20,13 +20,14 @@ const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
-  background: var(--clr-primary-10);
+  background: var(--clr-bg-1);
   text-align: center;
   h1 {
     font-size: 10rem;
+    color: var(--clr-heading-1);
   }
   h3 {
-    color: var(--clr-grey-3);
+    color: var(--clr-heading-2);
     margin-bottom: 1.5rem;
   }
 `;
